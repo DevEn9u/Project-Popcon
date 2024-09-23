9e3fb20d3e31402304c079333bbc4056256e92ab
DevEn <kangingoo31@gmail.com>
2024-09-23 17:35:25 +0900

    24.09.23(월)_중간 커밋

--- src/main/java/com/edu/springboot/MainController.java
+++ src/main/java/com/edu/springboot/MainController.java
@@ -2,6 +2,7 @@ package com.edu.springboot;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
@@ -10,4 +11,30 @@ public class MainController {
 	public String home() {
 		return "home";
 	}
+	// 로그인
+	@GetMapping("/login.do")
+	public String loginGet() {
+		return "/members/login";
+	}
+	@PostMapping("/login.do")
+	public String loginPost() {
+		return "redirect:/";
+	}
+	
+	// 회원가입 - 메인
+	@GetMapping("/register.do")
+	public String registerMain() {
+		return "/members/register-main";
+	}
+	// 회원가입 - 개인회원
+	@GetMapping("/register/normal.do")
+	public String registerNormal() {
+		return "/members/register-normal";
+	}
+	// 회원가입 - 기업회원
+	@GetMapping("/register/corp.do")
+	public String registerCorp() {
+		return "/members/register-corp";
+	}
+	
 }
