6f5be49ef7f3a36292394165cfac66dab9f3df98
dudms100 <qye215@gmail.com>
2024-10-11 14:41:25 +0900

    24.10.11 박여은 플러터앱에서 자유게시판 목록&상세보기(이미지제외)구현

--- src/main/java/com/edu/springboot/board/APIController.java
+++ src/main/java/com/edu/springboot/board/APIController.java
@@ -1,8 +1,9 @@
+// src/main/java/com/edu/springboot/board/APIController.java
+
 package com.edu.springboot.board;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,19 +19,15 @@ import com.edu.springboot.popupboards.CommentDTO;
 
 import lombok.RequiredArgsConstructor;
 
-@RestController // @Contoller + @RequesetBody 기능 제공
+@RestController // @Controller + @ResponseBody 기능 제공
 @RequiredArgsConstructor
 public class APIController {
 
-	@Autowired
-	private BoardService boardService;
-	@Autowired
-	private ImageService imageService;
-	@Autowired
-	private IMemberService memberService;
+    private final BoardService boardService;
+    private final ImageService imageService;
+    private final IMemberService memberService;
 
-
-	 // 자유게시판 목록
+    // 자유게시판 목록
     @GetMapping("/api/freeBoard/list")
     public ResponseEntity<List<BoardDTO>> listBoards(@RequestParam(value = "page", defaultValue = "1") int page) {
         List<BoardDTO> boardList = boardService.getFreeBoardsWithPaging(page);
@@ -46,7 +43,7 @@ public class APIController {
 
     // 자유게시판 상세보기
     @GetMapping("/api/freeBoard/view/{boardIdx}")
-    public ResponseEntity<BoardDTO> freeBoardView(@PathVariable String boardIdx) {
+    public ResponseEntity<BoardDTO> freeBoardView(@PathVariable("boardIdx") String boardIdx) {
         BoardDTO board = boardService.getBoardById(boardIdx);
         if (board == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
@@ -56,17 +53,6 @@ public class APIController {
         MemberDTO member = memberService.getMemberById(board.getWriter());
         String writerName = (member != null) ? member.getName() : "알 수 없음";
         board.setWriterName(writerName);
-        
-        // 관련 이미지 가져오기
-        List<ImageDTO> images = imageService.getImages(boardIdx, "BOARD");
-        
-        // 댓글 목록 가져오기
-        List<CommentDTO> comments = boardService.getComments(boardIdx);
-        for (CommentDTO comment : comments) {
-            MemberDTO coMember = memberService.getMemberById(comment.getCom_writer());
-            String comWriterName = (coMember != null) ? coMember.getName() : "알 수 없음";
-            comment.setComWriterName(comWriterName);
-        }
 
         return ResponseEntity.ok(board);
     }
